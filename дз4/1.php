2ad62560c4b4cf1fb02282ea56d0bd39d828ee10
ashar <nazarov_danik_01@mail.ru>
2022-04-16 18:46:36 +0400

    дз4

---
+++ дз4/1.php
@@ -0,0 +1,46 @@
+<?php
+$a = rand(1, 20);
+echo 'Наше число: ' . $a;
+switch ($a){
+    case 1:
+        echo '<br>1';
+    case 2:
+        echo '<br>2';
+    case 3:
+        echo '<br>3';
+    case 4:
+        echo '<br>4';
+    case 5:
+        echo '<br>5';
+    case 6:
+        echo '<br>6';
+    case 7:
+        echo '<br>7';
+    case 8:
+        echo '<br>8';
+    case 9:
+        echo '<br>9';
+    case 10:
+        echo '<br>10';
+    case 11:
+        echo '<br>11';
+    case 12:
+        echo '<br>12';
+    case 13:
+        echo '<br>13';
+    case 14:
+        echo '<br>14';
+    case 15:
+        echo '<br>15';
+    case 16:
+        echo '<br>16';
+    case 17:
+        echo '<br>17';
+    case 18:
+        echo '<br>18';
+    case 19:
+        echo '<br>19';
+    case 20:
+        echo '<br>20';
+
+}
